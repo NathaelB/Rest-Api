b97fb8cf400c061779a3c8c966ad1526dd597902
NathaelB <kirua48000@gmail.com>
2022-08-24 12:22:09 +0200

    :construction: add `UpdateValidator`

--- app/Validators/UserValidator.ts
+++ app/Validators/UserValidator.ts
@@ -18,3 +18,21 @@ export class StoreValidator {
 
   public messages: CustomMessages = {}
 }
+
+export class UpdateValidator {
+  constructor(protected ctx: HttpContextContract) {}
+  public schema = schema.create({
+    email: schema.string.optional({ trim: true }, [
+      rules.unique({
+        table: 'users', column: 'email'
+      }), 
+      rules.email()
+    ]),
+    username: schema.string.optional({ trim: true }, [rules.unique({
+      table: 'users', column: 'username'
+    })]),
+    password: schema.string.optional({ trim: true })
+  })
+
+  public messages: CustomMessages = {}
+}
