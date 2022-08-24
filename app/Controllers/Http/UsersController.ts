37e4fa10ad87118545b5084ba7073e8558049165
NathaelB <kirua48000@gmail.com>
2022-08-24 12:22:24 +0200

    :construction: add `update` & `destroy` method

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,6 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
-import { StoreValidator } from 'App/Validators/UserValidator'
+import { StoreValidator, UpdateValidator } from 'App/Validators/UserValidator'
 
 export default class UsersController {
     public async index ({ response }: HttpContextContract) {
@@ -22,6 +22,43 @@ export default class UsersController {
 
         return response.send({
             message: "Utilisateur créé",
+            user: user
+        })
+    }
+
+    public async update ({ params, request, response }: HttpContextContract) {
+        const user = await User.query().where('username', params.id).first()
+        if (!user) return response.send({message: "Utilisateur non existant"})
+
+        const data = await request.validate(UpdateValidator)
+        if (Object.keys(data).length === 0) {
+            return response.send({
+                message: `L'utilisateur ${user.id} n'a pas été modifié`,
+                user: user
+            }) 
+        }
+
+        await user.merge(data).save()
+        console.log(user);
+        
+        return response.send({
+            message: `L'utilisateur ${user.id} viens d'être modifié`,
+            user: user
+        })
+    }
+
+    public async destroy ({ params, response }: HttpContextContract) {
+        const user = await User.query().where('username', params.id).first()
+
+        if (!user) {
+            return response.send({
+                message: `L'utilisateur ${params.id} est inexistant`
+            })
+        }
+
+        await user.delete()
+        return response.send({
+            message: `L'utilisateur ${params.id} viens d'être anéhanti ^^`,
             user: user
         })
     }
