636cf18ff86ba2601aa0b27ad648099d0e921529
NathaelB <kirua48000@gmail.com>
2022-08-24 15:59:57 +0200

    :sparkles: init `AuthController`

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,37 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { LoginValidator } from 'App/Validators/UserValidator'
+
+export default class AuthController {
+	public async login ({ auth, request, response }: HttpContextContract) {
+		const credentials = await request.validate(LoginValidator)
+
+		try {
+			const token = await auth.use('api')
+				.attempt(credentials.email, credentials.password)
+			return response.send({
+				user: auth.user,
+				auth: token
+			})
+		} catch {
+			return response.badRequest('Invalid Credentials')
+		}
+	}
+
+	public async logout ({ auth, response }: HttpContextContract) {
+		await auth.use('api').revoke()
+		return response.send({
+			revoked: true
+		})
+	}
+
+	public async me ({ auth, response}: HttpContextContract) {
+		const user = auth.user
+
+    await user?.load('roles')
+    await user?.load('permissions', (query) => {
+      query.select('id', 'identifier', 'permission_group_id')
+    })
+
+    return response.send({ user: user })
+	}
+}
