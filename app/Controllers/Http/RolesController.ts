ccbdab9a38fc20b4c83601be52f2cb4cd85a4864
NathaelB <kirua48000@gmail.com>
2022-08-25 22:01:11 +0200

    :construction: update role validator & controller

--- app/Controllers/Http/RolesController.ts
+++ app/Controllers/Http/RolesController.ts
@@ -1,3 +1,66 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Role from 'App/Models/Role'
+import { StoreValidator, UpdateValidator } from 'App/Validators/RoleValidator'
 
-export default class RolesController {}
+
+export default class RolesController {
+	public async index ({ bouncer, response }: HttpContextContract) {
+		await bouncer.with('RolePolicy').authorize('viewList')
+		const roles = await Role.query().orderBy('label', 'desc')
+
+		return response.send({
+			number: roles.length,
+			roles: roles
+		})
+	}
+
+	public async show ({ bouncer, params, response}: HttpContextContract) {
+		await bouncer.with('RolePolicy').authorize('view')
+		const role = await Role.query().where('slug', params.id).first()
+
+		return response.send({ role: role })
+	}
+
+	public async store ({ request, bouncer, response }: HttpContextContract) {
+		await bouncer.with('RolePolicy').authorize('create')
+
+		const data = await request.validate(StoreValidator)
+
+		const role = await Role.create(data)
+		return response.send({ role: role })
+	}
+
+	public async update ({ request, params, bouncer, response }: HttpContextContract) {
+		await bouncer.with('PermissionPolicy').authorize('update')
+		const role = await Role.query().where('slug', params.id).first()
+		if (!role) return response.send({ message: "Role non existant" })
+
+		const data = await request.validate(UpdateValidator)
+		if (Object.keys(data).length === 0) {
+			return response.send({
+				message: `Le Role ${role.label} n'a pas été modifié`,
+				role: role
+			}) 
+		}
+		await role.merge(data).save()
+
+		return response.send({
+			message: `Le Role ${role.label} viens d'être modifié`,
+			role: role
+		})
+	}
+
+	public async destroy ({ response, params, bouncer }: HttpContextContract) {
+    await bouncer.with('RolePolicy').authorize('delete')
+
+    const role = await Role.query()
+      .where('slug', params.id)
+      .firstOrFail()
+
+    await role.delete()
+    return response.send({
+			message: `Le Role ${params.id} viens d'être anéhanti ^^`,
+			role: role
+		})
+  }
+}
--- app/Validators/RoleValidator.ts
+++ app/Validators/RoleValidator.ts
@@ -12,3 +12,16 @@ export class StoreValidator {
 
   public messages: CustomMessages = {}
 }
+
+export class UpdateValidator {
+  constructor(protected ctx: HttpContextContract) {}
+  public schema = schema.create({
+    label: schema.string.optional({ trim: true }, [rules.unique({
+      table: 'roles', column: 'label'
+    })]),
+    power: schema.number.optional()
+  })
+
+  public messages: CustomMessages = {}
+}
+
